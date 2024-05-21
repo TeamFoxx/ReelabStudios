f73646c05fe53598381e6f45ef86a6fa3df59339
TeamFoxx <hoxha.a2004@gmail.com>
2024-05-21 22:35:16 +0200

    v1.7-pre

--- config.py
+++ config.py
@@ -10,7 +10,7 @@
 # ⏤ { config } ⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤
 
 # Version information
-version = "v1.6-pre"
+version = "v1.7-pre"
 
 # List of Developer
 developer = {599204513722662933}
